82de12b1f7958957a4cc8dc7c7e6966868d09ebb
FelicityTech <souceking@gmail.com>
2024-08-12 11:47:14 +0100

    AJAX username validation

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,5 +4,5 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('register/',RegistrationView.as_view(), name="register" ),
-    path('validate_username/', csrf_exempt(UsernameValidationView.as_view()), name="validate_username"),
+    path('validate-username/', csrf_exempt(UsernameValidationView.as_view()), name="validate_username"),
 ]
